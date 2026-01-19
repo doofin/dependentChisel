c1fea95aac53fe982d2575da8bab9dac85888788
doofin <8177dph@gmail.com>
2026-01-19 18:31:15 +0800

    feat: Refactor checkUnInitAnalysis and remove checkUnInitLattice, integrating its functionality directly

--- src/main/scala/dependentChisel/staticAnalysis/checkUnInitAnalysis.scala
+++ src/main/scala/dependentChisel/staticAnalysis/checkUnInitAnalysis.scala
@@ -2,7 +2,6 @@ package dependentChisel.staticAnalysis
 
 import dependentChisel.codegen.sequentialCommands.AtomicCmds
 import dependentChisel.staticAnalysis.MonotoneFramework.domainMapT
-import dependentChisel.staticAnalysis.checkUnInitLattice
 
 import dependentChisel.staticAnalysis.MonotoneFramework.MonoFrameworkT
 import dependentChisel.codegen.sequentialCommands.NewInstance
@@ -12,7 +11,7 @@ import dependentChisel.codegen.sequentialCommands.VarDecls
 /** check if vars have an value
   */
 object checkUnInitAnalysis {
-  type mDomain = checkUnInitLattice.domain // which is Boolean
+  type mDomain = checkUnInitLattice.mDomain // which is Boolean
   type mStmt = AtomicCmds // statements
 
   // val init: mDomain = false
@@ -35,10 +34,26 @@ object checkUnInitAnalysis {
   ) extends MonoFrameworkT[mDomain, mStmt](
         transferF,
         mBotMap,
-        checkUnInitLattice.lattice
+        checkUnInitLattice
       ) {
 
     // override val baseLattice: semiLattice[mDomain] = uninitializedLattice.lattice //bug! will cause null
+
+  }
+
+  object checkUnInitLattice extends semiLattice[Boolean] {
+
+    override val smallerThan = {
+      case (_, true)      => true
+      case (false, false) => true
+      case _              => false
+    }
+
+    override val lub = {
+      _ || _
+    }
+
+    override val bottom = false
 
   }
 }
